e7151215985eda90effbf515a2ef0d3beea5a525
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-05-02 20:54:18 -0400

    firt projects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+import People from "./assets/people.svg";
+import Arrow from "./assets/arrow.svg";
+import Trash from "./assets/trash.svg";
+
+// SINTAX JSX
+import {
+  Container,
+  Image,
+  H1,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+  User,
+} from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const [name, setName] = useState();
+
+  const [age, setAge] = useState();
+
+  // REACT HOOKS => FERRAMENTAS AUXILIARES
+
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name, age }]);
+  }
+
+  function changeInputName(event) {
+    setName(event.target.value);
+  }
+
+  function changeInputAge(event) {
+    setAge(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="logo-imagem" src={People} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input onChange={changeInputName} placeholder="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input onChange={changeInputAge} placeholder="Idade" />
+
+        <Button onClick={addNewUser}>
+          Cadastrar <img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button>
+                <img src={Trash} alt="lata-de-lixo" />
+              </button>
+            </User>
+          ))}
+        </ul>
+      </ContainerItens>
+    </Container>
   );
 }
 
